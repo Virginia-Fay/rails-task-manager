0378636d6b82f486cffcb240638dbd3ffaae0a35
virginie lty <virginielty@gmail.com>
2021-12-11 15:29:13 +0100

    add a new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # list tasks
   get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  # add a new task  (Methode new avant read(id))
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # view the details of a task
+  get "tasks/:id", to: "tasks#show", as: :task
+
 end
