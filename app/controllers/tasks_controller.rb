6f3f97334634af2d3a128f48f1bb35fc74988eb9
virginie lty <virginielty@gmail.com>
2021-12-11 16:31:11 +0100

    update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,12 +14,29 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params) #instancie une Task avec les donnees du user
     @task.save
+    redirect_to tasks_path  #redirige vers la page qui affiche toutes les tasks
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
     redirect_to tasks_path
   end
 
   private
 
   def task_params
+    # paramètres autorisés dans formulaire
     params.require(:task).permit(:title, :details, :completed)
   end
 end
